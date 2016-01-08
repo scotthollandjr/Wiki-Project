94e13eb5cf7cc099f52b86104f79bf1609ed5243
Scott Holland <scotthollandjr@gmail.com>
2016-01-07 23:51:14 -0800

    Add hover code snippets

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+  $("li#html").hover(function() {
+    $("#htmlcode").toggle();
+  });
+  $("li#css").hover(function() {
+    $("#csscode").toggle();
+  });
+  $("li#javascript").hover(function() {
+    $("#scriptcode").toggle();
+  });
+  $("li#jquery").hover(function() {
+    $("#jquerycode").toggle();
+  });
 });
