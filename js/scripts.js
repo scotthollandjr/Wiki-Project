b7da52da209cb4df7107d2a6d7edb181658cfabd
Scott Holland <scotthollandjr@gmail.com>
2016-01-08 22:26:25 -0800

    Set up code block class

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
   $("li#html").hover(function() {
+    $("#homecode").toggle();
     $("#htmlcode").toggle();
   });
   $("li#css").hover(function() {
+    $("#homecode").toggle();
     $("#csscode").toggle();
   });
   $("li#javascript").hover(function() {
+    $("#homecode").toggle();
     $("#scriptcode").toggle();
   });
   $("li#jquery").hover(function() {
+    $("#homecode").toggle();
     $("#jquerycode").toggle();
   });
 });
